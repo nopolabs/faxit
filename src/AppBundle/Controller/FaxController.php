5eb63e59b9c8aaa83d7e59b960cceae1b989a04d
Dan Revel <dan@nopolabs.com>
2017-04-18 17:50:20 -0700

    render pdf preview

--- src/AppBundle/Controller/FaxController.php
+++ src/AppBundle/Controller/FaxController.php
@@ -3,12 +3,16 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Fax;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class FaxController extends Controller
 {
@@ -43,12 +47,22 @@ class FaxController extends Controller
             /** @var Fax $fax */
             $fax = $form->getData();
 
+            $params = [
+                'text' => $fax->getText(),
+            ];
+
+            $html = $this->getTwig()->render('fax/template.html.twig', $params);
+
             if ($form->get('html')->isClicked()) {
-                $view = 'HTML: ';
+                return new Response($html);
             } elseif ($form->get('pdf')->isClicked()) {
-                $view = 'PDF: ';
+                $pdf = $this->htmlToPdf($html);
+                return new Response($pdf, 200, [
+                    'Content-Type'        => 'application/pdf',
+                    'Content-Disposition' => 'attachment; filename="fax.pdf"',
+                ]);
             } elseif ($form->get('fax')->isClicked()) {
-                $view = 'FAX: ';
+                $view = 'FAX';
             };
 
             return $this->render('fax/template.html.twig', array(
@@ -59,5 +73,30 @@ class FaxController extends Controller
         return $this->render('fax/form.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+    public function htmlToPdf($html, Options $options = null)
+    {
+        $dompdf = $this->newDompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+
+        return $dompdf->output();
+    }
+
+    protected function newDompdf(Options $options = null) : Dompdf
+    {
+        if ($options === null) {
+            $options = new Options();
+            $options->set('defaultFont', 'Courier');
+        }
+
+        return new Dompdf($options);
+    }
+
+
+    protected function getTwig() : TwigEngine
+    {
+        return $this->container->get('templating');
     }
 }
